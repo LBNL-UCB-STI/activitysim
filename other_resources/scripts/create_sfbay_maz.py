701fea5795887b1707c3be3790c52422e6ebe494
Zach Needell <zaneedell@lbl.gov>
2024-07-25 17:21:15 -0700

    update maz inputs

--- other_resources/scripts/create_sfbay_maz.py
+++ other_resources/scripts/create_sfbay_maz.py
@@ -31,6 +31,7 @@ taz_missing['area_acres'] = taz_missing.geometry.area * 0.000247105
 taz_missing.geometry = taz_missing.geometry.centroid
 
 joined_sub_extended = pd.concat([joined_sub, taz_missing], axis=0).fillna(0.0).reset_index(drop=True)
+joined_sub_extended.rename_axis('MAZ', inplace=True)
 joined_sub_extended.to_csv("../../activitysim/examples/prototype_mtc_2_zone/data/maz.csv", float_format='%.4f')
 
 required_columns = ["TAZ", "DISTRICT", "SD", "COUNTY", "TOTHH", "TOTPOP", "TOTACRE", "RESACRE", "CIACRE", "TOTEMP",
@@ -45,21 +46,21 @@ def balltree_distance_matrix(a, b, max_distance, output_type="dok_matrix"):
     return ind, dist
 
 
-out = joined_sub_extended.geometry.apply(lambda x: (x.x, x.y)).to_list()
-inds, dists = balltree_distance_matrix(out, out, max_distance=1000)
-
-maz_to_maz = []
-for from_idx, (ind, dist) in enumerate(zip(inds, dists)):
-    maz_to_x = joined_sub_extended.iloc[ind].index.rename('DMAZ').to_frame().reset_index(drop=True)
-    maz_to_x['OMAZ'] = from_idx
-    maz_to_x['DISTWALK'] = dist.clip(200, None) / 1609.34
-    maz_to_maz.append(maz_to_x)
-
-maz_to_maz_df = pd.concat(maz_to_maz, axis=0)
-maz_to_maz_df.to_csv("../../activitysim/examples/prototype_mtc_2_zone/data/maz_to_maz_walk.csv.gz", float_format='%.3f', index=False)
-maz_to_maz_df.rename(columns={"DISTWALK": "DISTBIKE"}).to_csv(
-    "../../activitysim/examples/prototype_mtc_2_zone/data/maz_to_maz_bike.csv.gz", float_format='%.3f', index=False)
-print('DONE')
+# out = joined_sub_extended.geometry.apply(lambda x: (x.x, x.y)).to_list()
+# inds, dists = balltree_distance_matrix(out, out, max_distance=1000)
+#
+# maz_to_maz = []
+# for from_idx, (ind, dist) in enumerate(zip(inds, dists)):
+#     maz_to_x = joined_sub_extended.iloc[ind].index.rename('DMAZ').to_frame().reset_index(drop=True)
+#     maz_to_x['OMAZ'] = from_idx
+#     maz_to_x['DISTWALK'] = dist.clip(200, None) / 1609.34
+#     maz_to_maz.append(maz_to_x)
+#
+# maz_to_maz_df = pd.concat(maz_to_maz, axis=0)
+# maz_to_maz_df.to_csv("../../activitysim/examples/prototype_mtc_2_zone/data/maz_to_maz_walk.csv.gz", float_format='%.3f', index=False)
+# maz_to_maz_df.rename(columns={"DISTWALK": "DISTBIKE"}).to_csv(
+#     "../../activitysim/examples/prototype_mtc_2_zone/data/maz_to_maz_bike.csv.gz", float_format='%.3f', index=False)
+# print('DONE')
 
 pop_columns = ['TOTHH', 'TOTPOP', 'EMPRES', 'HHINCQ1',
                'HHINCQ2', 'HHINCQ3', 'HHINCQ4', 'AGE0004', 'AGE0519', 'AGE2044',
@@ -77,22 +78,25 @@ area_columns = ['TOTACRE']
 
 def divideTaz(tazIdx, mazIdxs):
     taz_sub = lu_orig.loc[lu_orig.TAZ == tazIdx]
-    maz_sub = joined_sub.loc[mazIdxs].copy()
-    maz_sub["pop_probs"] = maz_sub['Population'] / maz_sub['Population'].sum()
-    maz_sub["area_probs"] = maz_sub['area_acres'] / maz_sub['area_acres'].sum()
-    for col in pop_columns:
-        maz_sub[col] = np.round(taz_sub[col].values[0] * maz_sub['pop_probs']).astype(int)
-    for col in area_columns:
-        maz_sub[col] = taz_sub[col].values[0] * maz_sub['area_probs']
-    for col in copy_cols:
-        maz_sub[col] = taz_sub[col].values[0]
-    for num, col in density_cols.items():
-        maz_sub[col] = maz_sub[num] / maz_sub["TOTACRE"].values[0]
-    return maz_sub
+    maz_sub = joined_sub_extended.loc[mazIdxs].copy()
+    if maz_sub.shape[0] > 0:
+        maz_sub["pop_probs"] = (maz_sub['Population'] / maz_sub['Population'].sum()).fillna(1.0)
+        maz_sub["area_probs"] = (maz_sub['area_acres'] / maz_sub['area_acres'].sum()).fillna(1.0)
+        for col in pop_columns:
+            maz_sub[col] = np.round(taz_sub[col].values[0] * maz_sub['pop_probs']).astype(int)
+        for col in area_columns:
+            maz_sub[col] = taz_sub[col].values[0] * maz_sub['area_probs']
+        for col in copy_cols:
+            maz_sub[col] = taz_sub[col].values[0]
+        for num, col in density_cols.items():
+            maz_sub[col] = maz_sub[num] / maz_sub["TOTACRE"].values[0]
+        return maz_sub
+    else:
+        return taz_sub
 
 
 out = []
-for tazId, mazIds in joined_sub.groupby('taz1454').groups.items():
+for tazId, mazIds in joined_sub_extended.groupby('taz1454').groups.items():
     out.append(divideTaz(tazId, mazIds))
 
 
@@ -103,7 +107,10 @@ new_lu.drop(columns=['taz1454'], inplace=True)
 
 
 lu_missing = lu_orig.loc[~lu_orig.TAZ.isin(joined_sub.taz1454.unique()), :]
-lu_full = pd.concat([new_lu, lu_missing])
+lu_missing.reset_index(inplace=True)
+lu_missing.index = lu_missing.index + new_lu.index.max()
+lu_missing.rename_axis('MAZ', inplace=True)
+lu_full = pd.concat([new_lu.sort_index(), lu_missing])
 
 
 
@@ -117,12 +124,11 @@ too_far = dist_to_nearest_stop > 1609.34
 dist_to_nearest_stop /= 1609.34
 dist_to_nearest_stop[too_far] = -1
 # TODO: Make columns access_dist_transit with distance in miles to nearest transit station
-lu_full['access_dist_transit'] = dist_to_nearest_stop
+lu_full['access_dist_transit'] = -1.0
+lu_full['access_dist_transit'].values[:len(dist_to_nearest_stop)] = dist_to_nearest_stop
 for col in pop_columns:
     lu_full[col] = lu_full[col].astype(int)
-
-lu_full.drop(columns=['TAZ'], inplace=True)
-lu_full['TAZ'] = lu_full['taz1454'].astype(int)
+lu_full = lu_full.loc[~lu_full.index.duplicated()]
 lu_full.to_csv("../../activitysim/examples/prototype_mtc_2_zone/data/land_use.csv")
 
 
